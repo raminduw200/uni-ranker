1d91f116f2d6aa1f37c6be9a082b464662a6b9e7
Ramindu Walgama <raminduw200@gmail.com>
2022-07-02 11:08:03 +0530

    Add get_grade and modify get_sub_name to return subj name

--- Subject.py
+++ Subject.py
@@ -6,34 +6,41 @@ class Subject:
     def __init__(self, code_, grade_):
         self.code = code_
         self.grade = grade_
-        self.__set_name__(code_)
+        self.name = get_sub_name(code_)
 
-    def __set_name__(self, code_):
-        if code_ == "SCS 1201":
-            self.name = "Data Structures and Algorithms I"
-        elif code_ == "SCS 1202":
-            self.name = "Programming Using C"
-        elif code_ == "SCS 1203":
-            self.name = "Database I"
-        elif code_ == "SCS 1204":
-            self.name = "Discrete Mathematics I"
-        elif code_ == "SCS 1205":
-            self.name = "Computer Systems"
-        elif code_ == "SCS 1206":
-            self.name = "Laboratory I"
-        elif code_ == "SCS 1207":
-            self.name = "Software Engineering I"
-        elif code_ == "SCS 1208":
-            self.name = "Data Structures and Algorithms II"
-        elif code_ == "SCS 1209":
-            self.name = "Object Oriented Programming"
-        elif code_ == "SCS 1210":
-            self.name = "Software Engineering II"
-        elif code_ == "SCS 1211":
-            self.name = "Mathematical Methods I"
-        elif code_ == "SCS 1212":
-            self.name = "Foundation of Computer Science"
-        elif code_ == "SCS 1213":
-            self.name = "Probability and Statistics"
-        elif code_ == "SCS 1214":
-            self.name = "Operating Systems I"
+    def get_grade(self):
+        return self.grade
+
+    def __str__(self):
+        return f"{self.code} : {self.name} : {self.grade}"
+
+
+def get_sub_name(code_):
+    if code_ == "SCS 1201":
+        return "Data Structures and Algorithms I"
+    elif code_ == "SCS 1202":
+        return "Programming Using C"
+    elif code_ == "SCS 1203":
+        return "Database I"
+    elif code_ == "SCS 1204":
+        return "Discrete Mathematics I"
+    elif code_ == "SCS 1205":
+        return "Computer Systems"
+    elif code_ == "SCS 1206":
+        return "Laboratory I"
+    elif code_ == "SCS 1207":
+        return "Software Engineering I"
+    elif code_ == "SCS 1208":
+        return "Data Structures and Algorithms II"
+    elif code_ == "SCS 1209":
+        return "Object Oriented Programming"
+    elif code_ == "SCS 1210":
+        return "Software Engineering II"
+    elif code_ == "SCS 1211":
+        return "Mathematical Methods I"
+    elif code_ == "SCS 1212":
+        return "Foundation of Computer Science"
+    elif code_ == "SCS 1213":
+        return "Probability and Statistics"
+    elif code_ == "SCS 1214":
+        return "Operating Systems I"

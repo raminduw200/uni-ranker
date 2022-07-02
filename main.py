0de04105e2214977163abced227dc7c7c312ed89
Ramindu Walgama <raminduw200@gmail.com>
2022-07-02 11:08:43 +0530

    Create CSV

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from pathlib import Path
-
+import Subject
 from Student import Student
 
 STUDENTS = []
@@ -9,30 +9,41 @@ SHEETS_DIR = "Sheets/"
 
 def stu_exits(id_):
     for i in range(len(STUDENTS)):
-        if STUDENTS[i].id == id_:
+        if STUDENTS[i].get_id() == id_:
             return i
     return -1
 
 
 sheet_list = Path(SHEETS_DIR).rglob('*.csv')
+subjects_indices = []
+subjects_names = []
 for sheet in sheet_list:
     sheet = str(sheet)
 
-    df = pd.read_csv(sheet)
-    df = df[["Index", "Grade"]]
+    sheet_df = pd.read_csv(sheet)
+    sheet_df = sheet_df[["Index", "Grade"]]
 
-    subject = sheet[sheet.rfind('/') + 1: sheet.rfind('.')]
+    subject_code = sheet[sheet.rfind('/') + 1: sheet.rfind('.')]
+    subjects_indices.append(subject_code)
+    subjects_names.append(Subject.get_sub_name(subject_code))
 
-    for index, grade in df.itertuples(index=False):
+    for index, grade in sheet_df.itertuples(index=False):
         stu_index = stu_exits(index)
-        if not stu_index:  # student not exists
-            STUDENTS[stu_index].add_subject(subject, grade)
-        else:
+        if stu_index < 0:  # student not exists
             stu = Student(index)
-            stu.add_subject(subject, grade)
+            stu.add_subject(subject_code, grade)
             STUDENTS.append(stu)
+        else:
+            STUDENTS[stu_index].add_subject(subject_code, grade)
 
+result_df = pd.DataFrame(columns=["Index"] + subjects_names)
+for i in range(len(STUDENTS)):
+    row = [STUDENTS[i].id]
+    for j in range(len(subjects_indices)):
+        row.append(
+            STUDENTS[i].get_subject(subjects_indices[j]).get_grade() if STUDENTS[i].get_subject(subjects_indices[j])
+            else ""
+        )
+    result_df.loc[i] = row
 
-
-# for i in range(len(df)):
-#     print(f"{STUDENTS[i].id} : {STUDENTS[i].subjects[0].name} : {STUDENTS[i].subjects[0].grade}")
+result_df.to_csv("result.csv", index=False)

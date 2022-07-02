3877e4c446db26b5653fbca14851d0ad37bfbfeb
Ramindu Walgama <raminduw200@gmail.com>
2022-07-02 12:03:03 +0530

    Add GPV and GPA

--- Student.py
+++ Student.py
@@ -4,15 +4,23 @@ from Subject import Subject
 class Student:
     id: str = None
     # name: str = None
+    total_gpv: int = None
+    gpa: float = None
     subjects: [Subject] = None
+    TOTAL_CREDIT: int = 34
 
     def __init__(self, id_):
         self.id = id_
         # self.name = name
+        self.total_gpv = 0
+        self.gpa = 0
         self.subjects = []
 
     def add_subject(self, code_, grade_):
-        self.subjects.append(Subject(code_, grade_))
+        subj = Subject(code_, grade_)
+        self.subjects.append(subj)
+        self.total_gpv += subj.gpv
+        self.gpa = self.total_gpv / self.TOTAL_CREDIT
 
     def get_subject(self, code_):
         for subject in self.subjects:
--- Subject.py
+++ Subject.py
@@ -2,11 +2,73 @@ class Subject:
     code: str = None
     name: str = None
     grade: str = None
+    gpv: int = None
 
     def __init__(self, code_, grade_):
         self.code = code_
-        self.grade = grade_
         self.name = get_sub_name(code_)
+        self.grade = grade_
+        self.gpv = self.get_gpv(grade_) * self.get_credit_value(code_)
+
+    @staticmethod
+    def get_credit_value(code_):
+        if code_ == "SCS 1201":
+            return 3
+        elif code_ == "SCS 1202":
+            return 3
+        elif code_ == "SCS 1203":
+            return 3
+        elif code_ == "SCS 1204":
+            return 2
+        elif code_ == "SCS 1205":
+            return 2
+        elif code_ == "SCS 1206":
+            return 2
+        elif code_ == "SCS 1207":
+            return 2
+        elif code_ == "SCS 1208":
+            return 3
+        elif code_ == "SCS 1209":
+            return 3
+        elif code_ == "SCS 1210":
+            return 2
+        elif code_ == "SCS 1211":
+            return 2
+        elif code_ == "SCS 1212":
+            return 2
+        elif code_ == "SCS 1213":
+            return 2
+        elif code_ == "SCS 1214":
+            return 3
+
+    @staticmethod
+    def get_gpv(grade_):
+        if grade_ == "A+":
+            return 4
+        elif grade_ == "A":
+            return 4
+        elif grade_ == "A-":
+            return 3.7
+        elif grade_ == "B+":
+            return 3.3
+        elif grade_ == "B":
+            return 3.0
+        elif grade_ == "B-":
+            return 2.7
+        elif grade_ == "C+":
+            return 2.3
+        elif grade_ == "C":
+            return 2.0
+        elif grade_ == "C-":
+            return 1.7
+        elif grade_ == "D+":
+            return 1.3
+        elif grade_ == "D":
+            return 1.0
+        elif grade_ == "E":
+            return 0.0
+        elif grade_ == "F":
+            return 0.0
 
     def get_grade(self):
         return self.grade

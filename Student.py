8a422d10a28efed8d540ed800fccffd423d4c561
Ramindu Walgama <raminduw200@gmail.com>
2022-07-02 11:07:15 +0530

    Add get_subject by subj code and get_id method

--- Student.py
+++ Student.py
@@ -14,4 +14,12 @@ class Student:
     def add_subject(self, code_, grade_):
         self.subjects.append(Subject(code_, grade_))
 
+    def get_subject(self, code_):
+        for subject in self.subjects:
+            if subject.code == code_:
+                return subject
+        return None
+
+    def get_id(self):
+        return self.id
 

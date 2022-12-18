925bf89c307f03d1c997fda78be59410bc4aa1a7
Ramindu Walgama <raminduw200@gmail.com>
2022-12-18 11:18:13 +0530

    Remove hardcoded total credits

--- Student.py
+++ Student.py
@@ -7,7 +7,7 @@ class Student:
     total_gpv: int = None
     gpa: float = None
     subjects: [Subject] = None
-    TOTAL_CREDIT: int = 45
+    total_credit: int = 0
 
     def __init__(self, id_):
         self.id = id_
@@ -19,8 +19,9 @@ class Student:
     def add_subject(self, code_, grade_):
         subj = Subject(code_, grade_)
         self.subjects.append(subj)
+        self.total_credit += subj.credit
         self.total_gpv += subj.gpv
-        self.gpa = self.total_gpv / self.TOTAL_CREDIT
+        self.gpa = self.total_gpv / self.total_credit
 
     def get_subject(self, code_):
         for subject in self.subjects:
@@ -36,3 +37,6 @@ class Student:
 
     def get_gpa(self):
         return self.gpa
+
+    def __str__(self):
+        return f"{self.id} : {self.total_gpv} : {self.gpa}"
